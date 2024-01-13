b066cf79d9ac206c2a61e439eb91969192d71bae
Ted Lee <tedlee000@hotmail.com>
2024-01-13 14:36:22 -0500

    Initial Commit

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,21 @@
+//api calls
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+    export const shazamCoreApi = createApi({
+        reducerPath: 'shazamCoreApi',
+        baseQuery: fetchBaseQuery({
+            baseUrl: 'https://shazam.p.rapidapi.com',
+            prepareHeaders: (headers) => {
+                headers.set('X-RapidAPI-Key', 'ce17817db1msh0324966812d1a73p112faejsna2d68b6a41d5');
+
+                return headers;
+            },
+        }),
+        endpoints: (builder) => ({
+            getTopCharts: builder.query({ query: () => '/charts/track' }),
+        })
+    });
+
+    export const {
+        useGetTopChartsQuery,
+    } = shazamCoreApi;
